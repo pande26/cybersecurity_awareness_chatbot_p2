0a4d9cbb2977d851e6ce4b63a63c5140535c0e03
Pandelani Munenyiwa <200746245+pande26@users.noreply.github.com>
2026-05-23 12:51:36 +0200

    Added the username grid for the user to enter their name

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,6 +27,41 @@ namespace cybersecurity_awareness_chatbot_p2
 
         }//end of constructor
 
+        private void start_valerie(object sender, RoutedEventArgs e)
+        {//start of method
+
+            //set logo grid to hidden
+            logo_grid.Visibility = Visibility.Hidden;
+
+            //set username grid to visible
+            username_grid.Visibility = Visibility.Visible;
+
+        }//end of method
+
+        private void submit_username(object sender, RoutedEventArgs e)
+        {//start of submit_username() method
+
+            //collect user's input
+            string collected_name = user_name.Text.ToString();
+
+            //checking if the name is entered of not
+            if(collected_name != "")
+            {//start of if statement
+
+                //displaying the message if the user has entered a name
+                MessageBox.Show("Hey " + collected_name);
+
+            }//end of if statement
+            else
+            {//start of else statement
+
+                //error message
+                MessageBox.Show("Please enter your name...");
+
+            }//end of else statement
+
+        }//end of submit_username() method
+
     }//end of class
 
 }//end of namespace 
